46db7e1a10cc07f7c2afd2b255dec38a7ced6456
nicolasmanman <nicolas.manie@epfedu.fr>
2014-11-17 15:43:58 +0100

    First Commit v2
    Commit de mon travail en prenant en compte le git_ignore

---
+++ moteur-jeu/Assets/OpenDoor.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+	
+	[SerializeField]
+	public float moveSpeed;
+	public Rigidbody _porte;
+	public float ouverture; 
+
+	private float position_initiale;
+	private char orientation;
+	
+	/*
+		Use this for initialization
+	*/
+	void Start () 
+	{
+		if(_porte != null)
+		{
+			if(_porte.renderer.bounds.size.x > _porte.renderer.bounds.size.z) // Seulement les cas de portes coulissants sur l'axe des x ou z est pris en compte
+			{
+				orientation = 'x';
+				position_initiale = _porte.transform.position.x;
+			}
+			else 
+			{
+				orientation = 'z';
+				position_initiale = _porte.transform.position.z;
+			}
+		}
+    }
+
+
+	/*
+		UnTriggerStay is called at every frame while the player is in the trigger
+	*/
+	public void OnTriggerStay (Collider col) 
+	{
+		 if(_porte != null)
+		 {
+			if(Input.GetKey(KeyCode.P)) // INTEGRATION : récupérer le % d'ouverture et le stocker dans "long ouverture"
+			{
+				Debug.Log("getkey ");
+				if(orientation == 'x')
+				{
+					Debug.Log("DANS IF :"+orientation);
+					while(_porte.transform.position.x < position_initiale + (ouverture * _porte.renderer.bounds.size.x)) // tant qu'on n'a pas atteint la position finale
+					{	
+						Debug.Log("DANS WHILE :"+orientation);
+						_porte.transform.Translate(new Vector3(1, 0, 0) * moveSpeed * Time.deltaTime);
+					}
+					Debug.Log("APRES WHILE :"+orientation);
+				}
+
+				else if(orientation == 'z')
+				{
+					Debug.Log("DANS IF :"+orientation);
+					while(_porte.transform.position.z < position_initiale + (ouverture * _porte.renderer.bounds.size.z)) // tant qu'on n'a pas atteint la position finale
+					{	
+						Debug.Log("DANS WHILE :"+orientation);
+						_porte.transform.Translate(new Vector3(0, 0, 1) * moveSpeed * Time.deltaTime);
+					}
+				}
+
+				else 
+					Debug.LogError("Erreur d'orientation de la porte : doit etre orienté selon x OU z");
+			}
+		 }
+	}
+}
---
+++ moteur-jeu/Assets/OpenDoor2.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor2 : MonoBehaviour {
+	
+	[SerializeField]
+	public float moveSpeed;
+	public Rigidbody _porte;
+	public float ouverture; 
+	
+	private float position_initiale;
+	
+	/*
+		Use this for initialization
+	*/
+	void Start () 
+	{
+		if(_porte != null)
+			position_initiale = _porte.transform.position.y;
+	}
+	
+	
+	/*
+		UnTriggerStay is called at every frame while the player is in the trigger
+	*/
+	public void OnTriggerStay (Collider col) 
+	{
+		if(_porte != null)
+		{
+			if(Input.GetKey(KeyCode.P)) // INTEGRATION : récupérer le % d'ouverture et le stocker dans "long ouverture" On peut afficher un popup : "start movmnt now" pour freezer le jeu et acquerir les cpateurs
+			{
+				while(_porte.transform.position.y < position_initiale + (ouverture * _porte.renderer.bounds.size.y)) // tant qu'on n'a pas atteint la position finale
+					_porte.transform.Translate(new Vector3(0, 1, 0) * moveSpeed * Time.deltaTime); // déplacer la porte vers le haut
+			}
+		}
+	}
+}
