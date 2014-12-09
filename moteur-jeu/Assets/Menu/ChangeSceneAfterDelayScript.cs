d630418453483b6ba2be0a47957ca161d04ef46a
nicolasmanman <nicolas.manie@epfedu.fr>
2014-12-09 16:38:10 +0100

    Abandon Script Fade

--- moteur-jeu/Assets/Menu/ChangeSceneAfterDelayScript.cs
+++ moteur-jeu/Assets/Menu/ChangeSceneAfterDelayScript.cs
@@ -5,19 +5,12 @@ using System.Collections;
 public class ChangeSceneAfterDelayScript : MonoBehaviour {
 
 	public string _nextScene = "";
-	public float fadeSpeed = 1.5f;
 
 	public float _delay = 5f;
 
+	//public IEnumerator Start()
 	public IEnumerator Start()
 	{
-
-
-		GameObject panelObject = GameObject.Find("Fading");
-		Image panel = panelObject.GetComponent <Image> ();
-
-		panel.CrossFadeAlpha (255, fadeSpeed * Time.deltaTime, false);
-
 		yield return new WaitForSeconds (_delay); 
 
 		Application.LoadLevel (_nextScene);
