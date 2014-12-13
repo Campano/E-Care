b345f65800fca3574ca5a4b7cae8ee0aaa0a0e0a
nicolasmanman <nicolas.manie@epfedu.fr>
2014-12-13 15:10:26 +0100

    MainMenu

---
+++ moteur-jeu/Assets/Scripts/ChangeSceneOnClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneOnClick : MonoBehaviour {
+
+	public string _nextScene = "";
+
+	
+	// Update is called once per frame
+	public void Update () 
+	{
+		if (Input.GetMouseButtonDown (0) || (Input.touches != null && Input.touches.Length > 0))
+						Application.LoadLevel (_nextScene);
+	}
+}
---
+++ moteur-jeu/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneOnClick : MonoBehaviour {
+	
+	// Update is called once per frame
+	public void Update () 
+	{
+		if (Input.GetMouseButtonDown (0) || (Input.touches != null && Input.touches.Length > 0))
+						Application.Quit ();
+	}
+}
