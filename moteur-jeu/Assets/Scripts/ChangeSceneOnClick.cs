9e7d78c59cf36bbf4d28dbd063647b23d28186fe
nicolasmanman <nicolas.manie@epfedu.fr>
2014-12-13 16:35:23 +0100

    MainMenu suite

--- moteur-jeu/Assets/Scripts/ChangeSceneOnClick.cs
+++ moteur-jeu/Assets/Scripts/ChangeSceneOnClick.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class ChangeSceneOnClick : MonoBehaviour {
 
-	public string _nextScene = "";
-
+	public string nextScene = "";
 	
 	// Update is called once per frame
 	public void Update () 
 	{
-		if (Input.GetMouseButtonDown (0) || (Input.touches != null && Input.touches.Length > 0))
-						Application.LoadLevel (_nextScene);
+		if (Input.GetMouseButtonDown (0) || (Input.touches != null && Input.touches.Length > 0)) 
+		{
+			if (nextScene != "Quit")
+				Application.LoadLevel (nextScene);
+			else 
+				Application.Quit ();
+		}
 	}
 }
---
+++ moteur-jeu/Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	private Text text;
+	private Color basicColor;
+	public Color overColor;
+
+	private int basicSize;
+	public float ratioSize = 0.5f;
+
+
+	// Use this for initialization
+	void Awake () {
+		text = GetComponent<Text>();
+	
+		basicColor = text.color;
+		basicSize = text.fontSize;
+		print ("Color = " + basicColor);
+		print ("Size = " + basicSize);
+	}
+
+	void OnMouseEnter () {
+		print ("Coucou");
+		text.color = overColor;
+		text.fontSize = (int)(basicSize * ratioSize + basicSize);
+	}
+
+	void OnMouseExit () {
+		text.color = basicColor;
+		text.fontSize = basicSize;
+	}
+}
